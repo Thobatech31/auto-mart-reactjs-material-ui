1e9293dca5edffcd98bb4c86b86685a241a44cf4
Thobatech31 <owolabitoba31@gmail.com>
2022-05-07 05:15:14 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+
+const App = () => {
+  return(
+      <h1>Auto Mart is an online marketplace for automobiles, with Auto Mart users posting
+          their cars for sale.</h1>
+  )
+};
+
+export default App;
