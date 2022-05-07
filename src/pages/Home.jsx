d295225bf27cb5c13b7d762e780d7409a037ff03
Thobatech31 <owolabitoba31@gmail.com>
2022-05-07 15:51:08 +0000

    implement Car, carItem, cars components and Car page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import BannerSlider from "../components/Bannerslider";
-
+import Cars from "../components/Cars"
 const Home = () => {
     return (
         <div>
             <Navbar />
             <BannerSlider />
+            <Cars />
             <Footer />
         </div>
     );
